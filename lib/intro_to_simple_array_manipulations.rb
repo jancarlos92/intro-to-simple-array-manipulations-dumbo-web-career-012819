cd74b2a88b8bf00c5f37945b2a9b77c72493293f
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-11 13:49:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array,string) 
+    array = []
+    array.push("Niger") 
+    end 
+
+
+
+def using_unshift(array,string)
+  array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  array.unshift("Brooklyn Heights")
+  end
+  
+  def using_pop(array)
+    array = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  array.pop
+  end
+
+ def pop_with_args(array)
+   array = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+   until array.size.include?  
+   array.pop 
+ end 
+ 
