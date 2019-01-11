d25c3d292e19e3d106a6ba115a8662fbde89bb3a
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-11 13:58:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,12 +12,11 @@ def using_unshift(array,string)
   
   def using_pop(array)
     array = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-    array.pop
+    array.pop(-1)
   end
 
  def pop_with_args(array)
    array = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-   while array.size <= 2 
-   array.pop  
+    array.pop(-1,-2) 
  end 
  end
